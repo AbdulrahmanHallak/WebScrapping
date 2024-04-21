6d6c07425f73eb3cb67c093f58f062b00dc0ee7c
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-04-21 22:33:34 +0300

    parallelize scraping websites
    each scrape is independent so scraping with foreach slows
    the app uselessly

--- src/WebHaven.Api/Scrapables/Blogs/ScrapeBackgroundTask.cs
+++ src/WebHaven.Api/Scrapables/Blogs/ScrapeBackgroundTask.cs
@@ -9,27 +9,24 @@ internal class ScrapeBackgroundTask(IServiceScopeFactory scopeFactory) : Backgro
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            Console.WriteLine("Inside background task");
-
             using IServiceScope scope = scopeFactory.CreateScope();
+            var logger = scope.ServiceProvider.GetRequiredKeyedService<Logger<ScrapeBackgroundTask>>(typeof(Logger<ScrapeBackgroundTask>));
+            logger.LogInformation("Scraping websites");
+
             var repo = scope.ServiceProvider.GetRequiredService<BlogRepository>();
             var cache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
             var scrapper = scope.ServiceProvider.GetRequiredService<BlogScrapper>();
 
             ImmutableArray<BlogXPath> paths = await repo.GetBlogsXPath();
 
-            Dictionary<string, ImmutableArray<PostSummary>> result = [];
-            foreach (var item in paths)
-            {
-                ImmutableArray<PostSummary> posts = await scrapper.Scrape(item);
-                result.Add(item.Uri, posts);
-            }
+            var tasks = from path in paths
+                        select scrapper.Scrape(path);
+            var result = await Task.WhenAll(tasks);
 
             cache.CreateEntry(BlogConstants.BlogsCacheKey).AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1);
             cache.Set(BlogConstants.BlogsCacheKey, result);
 
-            Console.WriteLine("Added blogs to cache");
-
+            logger.LogInformation("Added blogs to cache");
             await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
         }
     }
