317145070207c65c23c1412f3d161c6e2f72214a
Abdulrahman Hallak <abdulrahmanhallak04@gmail.com>
2024-05-22 19:19:49 +0300

    fixed captive dependencies

--- src/WebHaven.Api/Scrapables/Blogs/ScrapeBackgroundTask.cs
+++ src/WebHaven.Api/Scrapables/Blogs/ScrapeBackgroundTask.cs
@@ -9,23 +9,25 @@ internal class ScrapeBackgroundTask(IServiceScopeFactory scopeFactory) : Backgro
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            using IServiceScope scope = scopeFactory.CreateScope();
-            var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger<ScrapeBackgroundTask>();
-            logger.LogInformation("Scraping websites");
+            using (IServiceScope scope = scopeFactory.CreateScope());
+            {
+                var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger<ScrapeBackgroundTask>();
+                logger.LogInformation("Scraping websites");
 
-            var repo = scope.ServiceProvider.GetRequiredService<BlogRepository>();
-            var cache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
-            var scrapper = scope.ServiceProvider.GetRequiredService<BlogScrapper>();
+                var repo = scope.ServiceProvider.GetRequiredService<BlogRepository>();
+                var cache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+                var scrapper = scope.ServiceProvider.GetRequiredService<BlogScrapper>();
 
-            ImmutableArray<BlogXPath> paths = await repo.GetBlogsXPath();
-            var scrapeTasks = paths.Select(path => new { path.Uri, Task = scrapper.Scrape(path) }).ToArray();
-            await Task.WhenAll(scrapeTasks.Select(x => x.Task));
+                ImmutableArray<BlogXPath> paths = await repo.GetBlogsXPath();
+                var scrapeTasks = paths.Select(path => new { path.Uri, Task = scrapper.Scrape(path) }).ToArray();
+                await Task.WhenAll(scrapeTasks.Select(x => x.Task));
 
-            foreach (var item in scrapeTasks)
-                cache.Set(item.Uri, await item.Task);
+                foreach (var item in scrapeTasks)
+                    cache.Set(item.Uri, await item.Task);
 
-            logger.LogInformation("Added blogs to cache");
+                logger.LogInformation("Added blogs to cache");
+            }
             await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
         }
     }
